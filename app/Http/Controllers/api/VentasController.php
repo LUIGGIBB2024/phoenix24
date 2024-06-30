96e27e67c88a849525e29007b3b1d4b53ea77c5d
LUIGGIBB2024 <enlacevisual@hotmail.com>
2024-06-30 14:28:53 -0500

    Update Consult Inventories

--- app/Http/Controllers/api/VentasController.php
+++ app/Http/Controllers/api/VentasController.php
@@ -910,7 +910,7 @@ class VentasController extends Controller
              'status'           => '200',
              'msg'              => 'Información de Saldos de Inventarios Año  ('. $anop .')',
              'data'             =>  $saldos,
-             'texto58'             =>  "Prueba",
+             'texto59'             =>  "Prueba",
             ],Response::HTTP_ACCEPTED);
 
     }
