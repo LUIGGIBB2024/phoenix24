15a53292650f56232329fe03ca3ffa3998dd289f
LUIGGIBB2024 <enlacevisual@hotmail.com>
2024-06-29 19:24:40 -0500

    Commit inicial

--- app/Http/Controllers/api/VentasController.php
+++ app/Http/Controllers/api/VentasController.php
@@ -905,7 +905,6 @@ class VentasController extends Controller
             ->orderBy('saldosdeinventarios.bodega')
             ->get();
 
-
         return response()->json(
             [
              'status'           => '200',
