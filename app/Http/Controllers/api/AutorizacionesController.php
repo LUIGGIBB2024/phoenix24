4607def4f7df410240ef13ff8b211b544aba196b
LUIGGIBB2024 <enlacevisual@hotmail.com>
2024-07-05 05:29:00 -0500

    Authorize Documents

--- app/Http/Controllers/api/AutorizacionesController.php
+++ app/Http/Controllers/api/AutorizacionesController.php
@@ -47,37 +47,19 @@ class AutorizacionesController extends Controller
                 ],Response::HTTP_ACCEPTED);
     }
 
-    public function AuthorizeDocuments():JsonResponse
+    public function AuthorizeDocuments(Request $request):JsonResponse
     {
-        $contador = 0;
 
-        $autorizaciones = autorizacion::select(
-            DB::raw("documentosautorizados.id as id"),
-            DB::raw("documentosautorizados.numerodedocumento as numerodedocumento"),
-            DB::raw("documentosautorizados.tipodedocumento as tipodedocumento"),
-            DB::raw("documentosautorizados.prefijo as prefijo"),
-            DB::raw("documentosautorizados.tipo as tipo"),
-            DB::raw("documentosautorizados.accion as accion"),
-            DB::raw("documentosautorizados.fechadereporte as fechadereporte"),
-            DB::raw("documentosautorizados.fechadesde as fechadesde"),
-            DB::raw("documentosautorizados.fechahasta as fechahasta"),
-            DB::raw("documentosautorizados.estado01 as estado01"),
-            DB::raw("documentosautorizados.estado02 as estado02"),
-            DB::raw("documentosautorizados.nit as nit"),
-            DB::raw("documentosautorizados.sucursal as sucursal"),
-            DB::raw("documentosautorizados.nombredeltercero as nombredeltercero"),
-            DB::raw("documentosautorizados.pin as pin"),
-            DB::raw("documentosautorizados.observaciones as observaciones"),
-            DB::raw("documentosautorizados.valordelafactura as valordelafactura"))
-            ->where('documentosautorizados.estado01','=', 1)
-            ->orderBy('documentosautorizados.fechadereporte','desc')
-            ->get();
+        $idpedido = $request;
 
-            return response()->json(
-                [
-                 'status'           => '200',
-                 'msg'              => 'COnsulta Exitosa',
-                 'datos'            => $autorizaciones
-                ],Response::HTTP_ACCEPTED);
+        $document  = autorizacion::find($idpedido)->get();
+
+
+        return response()->json(
+            [
+              'status'           => '200',
+              'msg'              => 'COnsulta Exitosa',
+              'datos'            => $document
+            ],Response::HTTP_ACCEPTED);
     }
 }
