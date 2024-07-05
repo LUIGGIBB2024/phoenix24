abce5975102ea8d7bf1e8fec0eef216a6db94fe7
LUIGGIBB2024 <enlacevisual@hotmail.com>
2024-07-05 05:09:59 -0500

    Authorize Documents

--- app/Http/Controllers/api/AutorizacionesController.php
+++ app/Http/Controllers/api/AutorizacionesController.php
@@ -13,6 +13,40 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AutorizacionesController extends Controller
 {
+    public function ConsultDocuments():JsonResponse
+    {
+        $contador = 0;
+
+        $autorizaciones = autorizacion::select(
+            DB::raw("documentosautorizados.id as id"),
+            DB::raw("documentosautorizados.numerodedocumento as numerodedocumento"),
+            DB::raw("documentosautorizados.tipodedocumento as tipodedocumento"),
+            DB::raw("documentosautorizados.prefijo as prefijo"),
+            DB::raw("documentosautorizados.tipo as tipo"),
+            DB::raw("documentosautorizados.accion as accion"),
+            DB::raw("documentosautorizados.fechadereporte as fechadereporte"),
+            DB::raw("documentosautorizados.fechadesde as fechadesde"),
+            DB::raw("documentosautorizados.fechahasta as fechahasta"),
+            DB::raw("documentosautorizados.estado01 as estado01"),
+            DB::raw("documentosautorizados.estado02 as estado02"),
+            DB::raw("documentosautorizados.nit as nit"),
+            DB::raw("documentosautorizados.sucursal as sucursal"),
+            DB::raw("documentosautorizados.nombredeltercero as nombredeltercero"),
+            DB::raw("documentosautorizados.pin as pin"),
+            DB::raw("documentosautorizados.observaciones as observaciones"),
+            DB::raw("documentosautorizados.valordelafactura as valordelafactura"))
+            ->where('documentosautorizados.estado01','=', 1)
+            ->orderBy('documentosautorizados.fechadereporte','desc')
+            ->get();
+
+            return response()->json(
+                [
+                 'status'           => '200',
+                 'msg'              => 'COnsulta Exitosa',
+                 'datos'            => $autorizaciones
+                ],Response::HTTP_ACCEPTED);
+    }
+
     public function AuthorizeDocuments():JsonResponse
     {
         $contador = 0;
@@ -45,5 +79,5 @@ class AutorizacionesController extends Controller
                  'msg'              => 'COnsulta Exitosa',
                  'datos'            => $autorizaciones
                 ],Response::HTTP_ACCEPTED);
-        }
+    }
 }
