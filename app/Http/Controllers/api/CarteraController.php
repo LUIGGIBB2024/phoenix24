a8ff27a5678a48bfa240d8edaa59d40d07d9c09e
LUIGGIBB2024 <enlacevisual@hotmail.com>
2024-07-07 22:51:56 -0500

    Updated Process Cxc

--- app/Http/Controllers/api/CarteraController.php
+++ app/Http/Controllers/api/CarteraController.php
@@ -156,7 +156,7 @@ class CarteraController extends Controller
     {
         $lapso = $request->lapso;
         $cartera = cartera::selectRaw("clientes.nombrecompleto, SUM(cuentasporcobrar.valorfactura) as total")
-                   ->sum('detalledepagoscxc.valor')
+                   ->selectRaw('sum(detalledepagoscxc.valor)')
                    ->join("clientes",function($join)
                     {
                       $join->on("clientes.nit","=","cuentasporcobrar.nit")
