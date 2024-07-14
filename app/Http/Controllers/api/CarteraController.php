f4d0ff2f2a0247c61817c77b3d39b267daabfc22
LUIGGIBB2024 <enlacevisual@hotmail.com>
2024-07-14 16:11:23 -0500

    Updated Process CxC

--- app/Http/Controllers/api/CarteraController.php
+++ app/Http/Controllers/api/CarteraController.php
@@ -229,7 +229,7 @@ class CarteraController extends Controller
                 ->where('cuentasporcobrar.fechafactura','<=',$fechacorte)
                 ->groupBy('cuentasporcobrar.cuentasporcobrarid')
                 ->orderBy('clientes.nombrecompleto')
-                ->havingRaw('total <> abonos || is_null(abonos)')
+                ->havingRaw('(total <> abonos || total <> is_null(abonos))')
                 ->get();
 
            $totalcartera = 0;
