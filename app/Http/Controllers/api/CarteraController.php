9dfb16dffdc077e072a5ba9b47a43ea059858df8
LUIGGIBB2024 <enlacevisual@hotmail.com>
2024-07-13 21:22:58 -0500

    Updated Process Cxc

--- app/Http/Controllers/api/CarteraController.php
+++ app/Http/Controllers/api/CarteraController.php
@@ -159,7 +159,7 @@ class CarteraController extends Controller
 
         $pagos = detalledepago::selectRaw('detalledepagoscxc.nit, detalledepagoscxc.sucursal,detalledepagoscxc.facturacxcID')
                 ->selectRaw('sum(detalledepagoscxc.valor) as abonos')
-                ->groupBy(['detalledepagoscxc.nit', 'detalledepagoscxc.sucursal'])
+                ->groupBy(['detalledepagoscxc.facturacxcID'])
                 ->get();
 
           // return response()->json(
