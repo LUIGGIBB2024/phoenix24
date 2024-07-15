9c45196d703c3de2f7dff8921814c4f1b47210f2
LUIGGIBB2024 <enlacevisual@hotmail.com>
2024-07-14 19:51:18 -0500

    Updated Process CxC

--- app/Http/Controllers/api/CarteraController.php
+++ app/Http/Controllers/api/CarteraController.php
@@ -217,7 +217,8 @@ class CarteraController extends Controller
 
         $cartera = cartera::selectRaw("clientes.nombrecompleto, cuentasporcobrar.fechafactura, cuentasporcobrar.fechadevencimiento")
                 ->selectRaw("cuentasporcobrar.numerodefactura,cuentasporcobrar.prefijo,cuentasporcobrar.tipodedocumento")
-                ->selectRaw("DATEDIFF($fechacorte,'cuentasporcobrar.fechadevencimiento') as Dias")
+                //->selectRaw("DATEDIFF($fechacorte,'cuentasporcobrar.fechadevencimiento') as Dias")
+                ->selectRaw("DATEDIFF(" . $fechacorte .",". $fechacorte .") as Dias")
                 ->selectRaw('cuentasporcobrar.cuentasporcobrarid')
                 ->selectRaw("cuentasporcobrar.valorfactura as total, pagos.abonos as abonos")
                 ->selectRaw("0.00 as saldo")
