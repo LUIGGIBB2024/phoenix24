064d7ee059c96b369ef306d1c982d4d635ccd820
LUIGGIBB2024 <enlacevisual@hotmail.com>
2024-07-16 20:41:13 -0500

    Consult Sales Enlace

--- app/Http/Controllers/api/VentasController.php
+++ app/Http/Controllers/api/VentasController.php
@@ -1131,7 +1131,7 @@ class VentasController extends Controller
             ->get();
 
 
-        $consolidado = $ventas->select('centrodeoperacion','fechafactura','totalventas')->groupBy(['centrodeoperacion','fechafactura']);
+        $consolidado = $ventas->collect('centrodeoperacion','fechafactura','totalventas')->groupBy(['centrodeoperacion','fechafactura']);
 
         return response()->json(
             [
