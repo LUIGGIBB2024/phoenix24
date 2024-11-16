fb779814ce8223677dff73e4585f834f2988e506
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2024-11-15 22:18:11 -0500

    Updated Inventories EnlaceVisual

--- resources/views/inventarios/saldos.blade.php
+++ resources/views/inventarios/saldos.blade.php
@@ -99,7 +99,7 @@
     <div class="row">
         @foreach ($productos as $producto)
             <div class="col-md-4">
-                <div class="card mb-4 card-personalizada p-3">
+                <div class="card mb-4 card-personalizada">
                     <div class="card-body">
                         <h5 class="card-title">{{ $producto->codigo}}</h5>
                         <p class="card-text parrafo">{{ $producto->descripciondelproducto}}</p>
