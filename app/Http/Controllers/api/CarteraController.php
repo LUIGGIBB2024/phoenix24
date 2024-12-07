c166b5c0896e435b142d77c5d382034e801b2b4f
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2024-12-06 20:46:43 -0500

    Updated Cartera Phoenix

--- app/Http/Controllers/api/CarteraController.php
+++ app/Http/Controllers/api/CarteraController.php
@@ -266,7 +266,7 @@ class CarteraController extends Controller
            ->where('cuentasporcobrar.fechafactura','<=',$fechacorte)
            ->where('clientes.nombrecompleto', 'like', '%' . $name . '%')
            ->groupBy('clientes.nombrecompleto')
-           ->havingRaw('intval(misaldo) > 0')          
+           ->havingRaw('cast(misaldo as int) > 0')          
            ->get();
 
            $totalcartera = 0;
