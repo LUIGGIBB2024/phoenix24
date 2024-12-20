eca660e3abdfe8acd9d97420d0ecdab746e932d7
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2024-12-19 21:22:05 -0500

    Updated CxP Phoenix

--- app/Models/detalledeotrospago.php
+++ app/Models/detalledeotrospago.php
@@ -13,7 +13,7 @@ class detalledeotrospago extends Model
     protected $fillable = [
      'consecutivo','tipodocumento','fechadocumento','lapso','nit','conceptodepago','sucursal','cuenta','centro','scentro','tipodemovimiento',
      'placa','valordelpago','nittercero','sucursaltercero','observaciones','numerorecibodecaja','docrecibodecaja','nitarrendatario',
-     'sucursalarrendatario','propiedad','estado','estado01','estado02','proyecto','sproyecto','centrooper','actividad',
+     'sucursalarrendatario','propiedad','estado','estado01','estado02','proyecto','sproyecto','centrooper','actividad','egresosid',
      'usuario_created','usuario_updated','fecha_created','fecha_updated'];
 
     protected $primaryKey = "detalledeotrospagosid";
