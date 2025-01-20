45d01e54f47a66de93f36f92a494133f21569e70
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-01-20 16:13:23 -0500

    Updated Documents Authorized

--- app/Models/autorizacion.php
+++ app/Models/autorizacion.php
@@ -12,7 +12,7 @@ class autorizacion extends Model
     protected $table = "documentosautorizados";
     protected $fillable = [
         'id','numerodedocumento','tipodedocumento','prefijo','tipo','accion','fechadereporte',
-        'fechadesde','fechahasta','estado01','estado02','nit','sucursal','nombredeltercero','valordelafactura',
+        'fechadesde','fechahasta','estado01','estado02','estado03','nit','sucursal','nombredeltercero','valordelafactura',
         'pin','observaciones','usuario_created','usuario_updated','fecha_created','fecha_updated'];
 
     //protected $primaryKey = "facturasid";
