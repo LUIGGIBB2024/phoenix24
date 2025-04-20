dcf1a68218422140ee1ef0fe68fd8f1a31a62338
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-04-20 11:02:11 -0500

    Update Pedidos

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -10,10 +10,10 @@ class Pedido extends Model
     use HasFactory;
     protected $table = "pedidos";
     protected $fillable = [
-        'consecutivo','tipodedocumento','fechadocumento','nit','sucursal','ncargue','horadepedido','nitaseguradora','sucursalaseg','nombreventa','numerodepoliza','numerodesiniestro','fechadeentrega','horadeentrega','direcciondeentrega','telefonodeentrega',
+        'consecutivo','tipodedocumento','fechadocumento','nit','sucursal','ncargue','horadepedido','nitaseguradora','sucursalaseg','nombreventa','nombres','apellidos','numerodepoliza','numerodesiniestro','fechadeentrega','horadeentrega','direcciondeentrega','telefonodeentrega',
         'reportedelcliente','observaciones','numerodeorden','vendedor','rutadeventa','zonadeventa','transportador','tipocliente','kilometraje','placa','numerodocumento','tipodepago','valorpedido','dsctosproductos','dsctosadicionales','lapso','valoriva',
         'retefuente','reteiva','reteica','valoradicional','totalpedido','costodeventa','valorotrodocumento','proyecto','sproyecto','centrooper','actividad','estado','estado01','estado02','reportedeltecnico','mesa','caja','cajero','tecnico','lista','latitud',
-        'longitud','usuario_created','usuario_updated','fecha_created','fecha_updated'];
+        'longitud','email','ciudad','usuario_created','usuario_updated','fecha_created','fecha_updated'];
 
     protected $primaryKey = "pedidosid";
     public $timestamps = false;
