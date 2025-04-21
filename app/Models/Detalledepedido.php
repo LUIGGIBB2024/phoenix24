f3a29b73e0b7d92018312cc7756007da0b792cfd
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-04-20 19:05:06 -0500

    Update DetPedidos

--- app/Models/Detalledepedido.php
+++ app/Models/Detalledepedido.php
@@ -10,7 +10,7 @@ class Detalledepedido extends Model
     use HasFactory;
     protected $table = "detalledelpedido";
     protected $fillable = [
-    'consecutivo','tipodedocumento','fechadocumento','nit','sucursal','proyecto','sproyecto','centrooper','lapso','nittercero','rutadeventa','zonadeventa','tipocliente','vendedor','lista','tecnico',
+    'consecutivo','pedidosid','tipodedocumento','fechadocumento','nit','sucursal','proyecto','sproyecto','centrooper','lapso','nittercero','rutadeventa','zonadeventa','tipocliente','vendedor','lista','tecnico',
     'serial','garantia','producto','bodega','lote','descripcion','concepto','cptoclase','ncargue','cantidad','cantidad2','valor','descuento1','descuento2','descuento3','iva','costopromedio','fechadevencimiento',
     'estado','estado01','estado02','valorreal','placa','actividad', 'PedidosID','usuario_created','usuario_updated','fecha_created','fecha_updated'];
 
