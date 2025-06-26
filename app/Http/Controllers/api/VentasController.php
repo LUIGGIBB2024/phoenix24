62c2938627904876ac596d59d55b3ad4e6eb7a95
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-06-26 13:23:32 -0500

    Updated General

--- app/Http/Controllers/api/VentasController.php
+++ app/Http/Controllers/api/VentasController.php
@@ -943,18 +943,6 @@ class VentasController extends Controller
             ->where('producto.codigo','=',$producto)->first();
 
 
-            //if (is_null($curproducto->porcentajeiva))
-            // if (is_null($curproducto))
-            // {
-            // return response()->json(
-            //     [
-            //      'status'   => '200 OK Detalle de lista',
-            //      'msg'      => 'Actualización Exitosa',
-            //      'producto' => $producto,
-            //      'Curproducto' => $curproducto,
-            //     ],Response::HTTP_ACCEPTED);
-            // }
-
             $porcentaje = is_null($curproducto)?0:$curproducto->porcentajeiva;
 
             $valorneto  = round($valor * (1  + ($porcentaje/100)),0);
